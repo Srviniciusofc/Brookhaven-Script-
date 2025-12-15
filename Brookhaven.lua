564148a76fe20dc60bbce0b70b98c2a2d3a43fe2
Vinícius <mv6969375@gmail.com>
2025-12-15 20:49:29 -0300

    update Brookhaven.lua

---
+++ Brookhaven.lua
@@ -0,0 +1,43 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/tlredz/Library/refs/heads/main/redz-V5-remake/main.luau"))()
+
+
+
+
+
+
+local Window = Library:MakeWindow({
+  Title = "Vini Hub : Brookhaven 🏡",
+  SubTitle = "dev by real_redz",
+  ScriptFolder = "redz-library-V5"
+})
+
+
+
+
+
+
+local Minimizer = Window:NewMinimizer({
+  KeyCode = Enum.KeyCode.LeftControl
+})
+
+local MobileButton = Minimizer:CreateMobileMinimizer({
+  Image = "rbxassetid://0",
+  BackgroundColor3 = Color3.fromRGB(98, 37, 209)
+})
+
+
+
+
+
+
+local Tab = Window:MakeTab({
+  Title = "Main",
+  Icon = "Home"
+})
+
+
+
+
+
+
+local Tab = Window:MakeTab({ "🥵", "Home" })
